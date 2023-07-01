64effa4c8af0e12f6cc5d8cc657d8a8b945695fb
MulindwaMarkMyles <mylesmark753@gmail.com>
2023-07-01 16:08:40 +0300

    first commit

---
+++ matrix/matrix.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ROWS 37
+#define COLS 148
+
+void set(char **array)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		array[i] = calloc(COLS, sizeof(char));
+		memset(array[i], ' ', COLS);
+	}
+}
+void draw(char **array)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			printf("\033[1;32m%c\033[1;0m", array[i][j]);
+		}
+		putchar('\n');
+	}
+}
+void assign(char **array)
+{
+	int x = rand() % ROWS;
+	int y = rand() % COLS;
+	int turn = 10;
+	int i = 0;
+	if (array[x][y] == ' ')
+	{
+		array[x][y] = 'x';
+		i++;
+	}
+}
+
+int main(void)
+{
+	char **array = malloc(ROWS + 1);
+	srand(time(NULL));
+	set(array);
+	while (1)
+	{
+		assign(array);
+		draw(array);
+	}
+	return (0);
+}
